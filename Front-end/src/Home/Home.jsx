eb26f9ff7e88462db2c2175850f25307c48471ab
Xembo <nishadvishal937@gmail.com>
2024-06-22 22:16:16 -0700

    sapration of conserns

--- Front-end/src/Home/Home.jsx
+++ Front-end/src/Home/Home.jsx
@@ -1,3 +1,4 @@
+// src/Home.js
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css';
@@ -15,7 +16,8 @@ import AddIcon from '@mui/icons-material/Add';
 import TaskModal from './TaskModal';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
-import axios from 'axios';
+import Sidebar from './Sidebar';
+import { fetchTasks, addTask as addTaskApi, deleteTask as deleteTaskApi, editTask as editTaskApi } from './APIs/api';
 
 const Home = () => {
   const [showNames, setShowNames] = useState(true);
@@ -37,8 +39,8 @@ const Home = () => {
   };
 
   const addTask = (newTask) => {
-    axios.post('http://localhost:3001/add', newTask)
-      .then(response => {
+    addTaskApi(newTask)
+      .then((response) => {
         const savedTask = response.data;
         setTasks((prevTasks) => ({
           ...prevTasks,
@@ -46,11 +48,11 @@ const Home = () => {
         }));
         setShowModal(false);
       })
-      .catch(error => console.error('Error adding task:', error));
+      .catch((error) => console.error('Error adding task:', error));
   };
 
   const deleteTask = (category, taskId) => {
-    axios.delete(`http://localhost:3001/delete/${taskId}`)
+    deleteTaskApi(taskId)
       .then(() => {
         const updatedTasks = tasks[category].filter((task) => task.id !== taskId);
         setTasks((prevTasks) => ({
@@ -58,12 +60,12 @@ const Home = () => {
           [category]: updatedTasks,
         }));
       })
-      .catch(error => console.error('Error deleting task:', error));
+      .catch((error) => console.error('Error deleting task:', error));
   };
 
   const editTask = (category, updatedTask) => {
-    axios.put(`http://localhost:3001/edit/${updatedTask.id}`, updatedTask)
-      .then(response => {
+    editTaskApi(updatedTask)
+      .then((response) => {
         const updatedTasks = tasks[category].map((task) =>
           task.id === updatedTask.id ? response.data : task
         );
@@ -72,19 +74,19 @@ const Home = () => {
           [category]: updatedTasks,
         }));
       })
-      .catch(error => console.error('Error editing task:', error));
+      .catch((error) => console.error('Error editing task:', error));
   };
 
   useEffect(() => {
-    axios.get('http://localhost:3001/tasks')
-      .then(response => {
+    fetchTasks()
+      .then((response) => {
         const tasks = response.data;
-        const today = tasks.filter(task => isToday(task.dueDate));
-        const week = tasks.filter(task => isThisWeek(task.dueDate));
-        const monthYear = tasks.filter(task => isThisMonthOrYear(task.dueDate));
+        const today = tasks.filter((task) => isToday(task.dueDate));
+        const week = tasks.filter((task) => isThisWeek(task.dueDate));
+        const monthYear = tasks.filter((task) => isThisMonthOrYear(task.dueDate));
         setTasks({ today, week, monthYear });
       })
-      .catch(error => console.error('Error fetching tasks:', error));
+      .catch((error) => console.error('Error fetching tasks:', error));
   }, []);
 
   const isToday = (dueDate) => {
@@ -110,68 +112,15 @@ const Home = () => {
   const isThisMonthOrYear = (dueDate) => {
     const today = new Date();
     const taskDate = new Date(dueDate);
-    return (taskDate.getMonth() === today.getMonth() && taskDate.getFullYear() === today.getFullYear()) || taskDate.getFullYear() === today.getFullYear();
+    return (
+      (taskDate.getMonth() === today.getMonth() && taskDate.getFullYear() === today.getFullYear()) ||
+      taskDate.getFullYear() === today.getFullYear()
+    );
   };
 
   return (
     <div className="home">
-      <div className={`sidebar ${!showNames ? 'hide-names' : ''}`}>
-        <nav>
-          <div className="sidebar-title">
-            <button className="toggle-button" onClick={toggleNames}>
-              ☰
-            </button>
-            {showNames && <h2>Task Manager</h2>}
-          </div>
-          <ul>
-            <li>
-              <Link to="/dashboard">
-                <DashboardIcon className="sidebar-icon" />
-                {showNames && 'Dashboard'}
-              </Link>
-            </li>
-            <li>
-              <Link to="/message">
-                <MessageIcon className="sidebar-icon" />
-                {showNames && 'Message'}
-              </Link>
-            </li>
-            <li>
-              <Link to="/tasks">
-                <TaskIcon className="sidebar-icon" />
-                {showNames && 'Tasks'}
-              </Link>
-            </li>
-            <li>
-              <Link to="/planning">
-                <PlanningIcon className="sidebar-icon" />
-                {showNames && 'Planning'}
-              </Link>
-            </li>
-            <li>
-              <Link to="/global">
-                <GlobalIcon className="sidebar-icon" />
-                {showNames && 'Global'}
-              </Link>
-            </li>
-            <li>
-              <Link to="/analytics">
-                <AnalyticsIcon className="sidebar-icon" />
-                {showNames && 'Analytics'}
-              </Link>
-            </li>
-            <li>
-              <Link to="/finance">
-                <FinanceIcon className="sidebar-icon" />
-                {showNames && 'Finance'}
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <div className="user-icon">
-          <PersonIcon className="img" />
-        </div>
-      </div>
+      <Sidebar showNames={showNames} toggleNames={toggleNames} />
       <div className="main">
         <div className="title-bar">
           <h2>Task-2Do</h2>
@@ -181,51 +130,58 @@ const Home = () => {
           </div>
         </div>
         <div className="task-columns">
-          <div className="task-column">
-            <h3>Today</h3>
-            <button className="add-task-button" onClick={() => handleAddTask('today')}>
-              <AddIcon />
-            </button>
-            {tasks.today.map(task => (
-              <div className="task" key={task.id}>
-                <h4>{task.title}</h4>
-                <p>{task.description}</p>
-                <button onClick={() => editTask('today', task)}><EditIcon /></button>
-                <button onClick={() => deleteTask('today', task.id)}><DeleteIcon /></button>
-              </div>
-            ))}
-          </div>
-          <div className="task-column">
-            <h3>This Week</h3>
-            <button className="add-task-button" onClick={() => handleAddTask('week')}>
-              <AddIcon />
-            </button>
-            {tasks.week.map(task => (
-              <div className="task" key={task.id}>
-                <h4>{task.title}</h4>
-                <p>{task.description}</p>
-                <button onClick={() => editTask('week', task)}><EditIcon /></button>
-                <button onClick={() => deleteTask('week', task.id)}><DeleteIcon /></button>
-              </div>
-            ))}
-          </div>
-          <div className="task-column">
-            <h3>This Month and This Year</h3>
-            <button className="add-task-button" onClick={() => handleAddTask('monthYear')}>
-              <AddIcon />
-            </button>
-            {tasks.monthYear.map(task => (
-              <div className="task" key={task.id}>
-                <h4>{task.title}</h4>
-                <p>{task.description}</p>
-                <button onClick={() => editTask('monthYear', task)}><EditIcon /></button>
-                <button onClick={() => deleteTask('monthYear', task.id)}><DeleteIcon /></button>
-              </div>
-            ))}
-          </div>
+          <TaskColumn
+            title="Today"
+            tasks={tasks.today}
+            handleAddTask={handleAddTask}
+            taskCategory="today"
+            editTask={editTask}
+            deleteTask={deleteTask}
+          />
+          <TaskColumn
+            title="This Week"
+            tasks={tasks.week}
+            handleAddTask={handleAddTask}
+            taskCategory="week"
+            editTask={editTask}
+            deleteTask={deleteTask}
+          />
+          <TaskColumn
+            title="This Month and This Year"
+            tasks={tasks.monthYear}
+            handleAddTask={handleAddTask}
+            taskCategory="monthYear"
+            editTask={editTask}
+            deleteTask={deleteTask}
+          />
         </div>
       </div>
       {showModal && <TaskModal onClose={closeModal} onSave={addTask} />}
+    </div>
+  );
+};
+
+const TaskColumn = ({ title, tasks, handleAddTask, taskCategory, editTask, deleteTask }) => {
+  return (
+    <div className={`task-column ${taskCategory}`}>
+      <h3>{title}</h3>
+      <button className="add-task-button" onClick={() => handleAddTask(taskCategory)}>
+        <AddIcon />
+      </button>
+      {tasks.map((task) => (
+        <div className="task" key={task.id}>
+          <h4>{task.title}</h4>
+          <p>{task.description}</p>
+          <div className="task-actions">
+            <button onClick={() => editTask(taskCategory, task)}>
+              <EditIcon />
+            </button>
+            <button onClick={() => deleteTask(taskCategory, task.id)}>
+              <DeleteIcon />
+            </button>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
